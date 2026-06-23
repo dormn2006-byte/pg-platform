a02fe2193bfe28dc641bc5937bd7425a2ebda498
Aditya40855 <adityasingh.aiml@gmail.com>
2026-06-23 10:54:21 +0530

    test

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,7 +8,7 @@ import bookingRoutes from "./routes/bookingRoutes.js";
 import superAdminRoutes from "./routes/superAdminRoutes.js";
 
 
-
+// deployment test
 dotenv.config();
 
 const app = express();
