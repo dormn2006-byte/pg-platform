7bdd40967e3270be3339fab762396d8b9807d5ca
Aditya40855 <adityasingh.aiml@gmail.com>
2026-06-28 20:47:39 +0530

    update image backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,14 +16,30 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
-app.use("/api/pg", pgRoutes);
-app.use("/uploads", express.static("src/uploads"));
+app.use("/api/pg", pgRoutes);import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log("Uploads folder:");
+console.log(path.join(__dirname, "uploads"));
+
+app.use(
+  "/uploads",
+  express.static(path.join(__dirname, "uploads"))
+);
+
 app.use("/api/bookings", bookingRoutes);
 app.use("/api/superadmin", superAdminRoutes);
 app.get("/", (req, res) => {
   res.send("PG Platform Backend Running");
 });
-
+app.get("/test-upload-path", (req, res) => {
+  res.json({
+      uploadFolder: path.join(__dirname, "uploads")
+  });
+});
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
